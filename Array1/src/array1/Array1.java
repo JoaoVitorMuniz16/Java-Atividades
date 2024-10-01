0fe98f3628a26d3e30040dfbe58aac67a94ebaab
João Vitor Muniz Vasconcelos <140927075+JoaoVitorMuniz16@users.noreply.github.com>
2024-10-01 18:14:25 -0300

    Add files via upload

---
+++ Array1/src/array1/Array1.java
@@ -0,0 +1,10 @@
+package array1;
+
+public class Array1 {
+
+    public static void main(String[] args) {
+        String[] carros = {"Fusca", "Brasilia", "Gurgel"};
+        System.out.println(carros[1]);
+    }
+    
+}
